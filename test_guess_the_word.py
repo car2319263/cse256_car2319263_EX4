6d92adb5f0bdb04fb5b80aa499558f4aa3fcfe97
Carla Aleman <car2319263@maricopa.edu>
2024-11-13 03:56:36 -0700

    Commit unit test

---
+++ test_guess_the_word.py
@@ -0,0 +1,13 @@
+import unittest
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+    def test_choose_word(self):
+        self.assertEqual(True, False)
+    def test_get_word(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
