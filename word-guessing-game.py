a045ab1e3680769cf982e6949ad3f374be843b1d
Carla Aleman <car2319263@maricopa.edu>
2024-11-13 14:13:13 -0700

    Fixed get_word and added invalid input checks

--- word-guessing-game.py
+++ word-guessing-game.py
@@ -4,22 +4,22 @@ def choose_word():
     words = ["computer", "python", "mesa", "programming", "game"]
     return random.choice(words)
 def get_word(word, guessed_lett):
-    out = ""
-    for letter in word:
-        if letter in guessed_lett:
-            out + letter
-        else: out + "_"
-    return out
+    return "".join([letter if letter in guessed_lett else "_" for letter in word])
 
 def game():
     word = choose_word()
     guessed = set()
-    attempts = 20
+    attempts = len(word) + 4
 
     print("Starting the Word Guessing Game")
     while attempts > 0:
         print("Guess the letter: ", end = " ")
         guess = input().lower()
+        if len(guess)!=1:
+            print("Invalid input.")
+        if guess in guessed:
+            print("Letter was already guessed.")
+            continue
         guessed.add(guess)
         if guess in word:
             print("Correct!")
@@ -28,10 +28,9 @@ def game():
             attempts -= 1
             print(f"You have {attempts} attempts left")
         print( get_word(word, guessed))
-
-
-    if "_" not in get_word(word, guessed):
-        print("You guessed it!")
+        if "_" not in get_word(word, guessed):
+            print(f"You guessed it!")
+            cont = True
     else:
         print(f"Game over... The word is {word}")
 
