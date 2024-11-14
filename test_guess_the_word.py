e26449dc9021da29666a6d0fb538b811bae98cea
Carla Aleman <car2319263@maricopa.edu>
2024-11-13 23:02:11 -0700

    Added comments to code

--- test_guess_the_word.py
+++ test_guess_the_word.py
@@ -1,16 +1,26 @@
+# Carla Aleman
+# CIS256 Fall 2024
+# EX4 (EX 4)
 import unittest
 from word_guessing_game import choose_word, get_word
 class MyTestCase(unittest.TestCase):
+    # Test choose word method to select word from list randomly
     def test_choose_word(self):
         words = ["computer", "python", "mesa", "programming", "game"]
         assert choose_word() in words
+    # Test get word to make sure output is correct
     def test_get_word(self):
+        # Some letters test
         word = "programming"
         letters = {'p','a', 'g'}
         assert get_word(word, letters) == 'p__g_a____g'
+        # All letters test
         word2 = "mesa"
         letters2 = {'m','e','s','a'}
         assert get_word(word2, letters2) == 'mesa'
+        # No letters tests
+        letters3 = {}
+        assert get_word(word2, letters3) == '____'
 
 if __name__ == '__main__':
     unittest.main()
--- word_guessing_game.py
+++ word_guessing_game.py
@@ -1,36 +1,50 @@
+# Carla Aleman
+# CIS256 Fall 2024
+# EX4 (EX 4)
 import random
-
+# Choose a random word from a lsit
 def choose_word():
     words = ["computer", "python", "mesa", "programming", "game"]
     return random.choice(words)
+# Get the word with the letters guessed, otherwise _
 def get_word(word, guessed_lett):
     return "".join([letter if letter in guessed_lett else "_" for letter in word])
-
+# Game logic
 def game():
+    # Choose a word, set the guessed letters list and attempts
     word = choose_word()
     guessed = set()
-    attempts = len(word) + 4
+    attempts = 15
 
     print("Starting the Word Guessing Game")
+    # While there is attempts left
     while attempts > 0:
+        # Prompt for letter
         print("Guess the letter: ", end = " ")
         guess = input().lower()
+        # If no letter, invalic
         if len(guess)!=1:
             print("Invalid input.")
+        # If letter was already used
         if guess in guessed:
             print("Letter was already guessed.")
             continue
+        # Add letter
         guessed.add(guess)
+        # Correct guess
         if guess in word:
             print("Correct!")
+        # If incorrect, lower the attempts
         else:
             print("Incorrect.", end = " ")
             attempts -= 1
             print(f"You have {attempts} attempts left")
-        print( get_word(word, guessed))
+        # Print the word if guessed
+        print(get_word(word, guessed))
         if "_" not in get_word(word, guessed):
-            print(f"You guessed it!")
+            print(f"Congratulations! You guessed it!")
             break
+    # If not guessed in the attempts, end game
     else:
         print(f"Game over... The word is {word}")
 
